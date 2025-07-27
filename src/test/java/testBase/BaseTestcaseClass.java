4a48eace88604e2577e66b11098c09fd0ac04d29
Ronak010325 <ronakyadav1325@gmail.com>
2025-07-27 17:20:15 +0530

    Added the remote execution with selenium grid not implemented docker yet

--- src/test/java/testBase/BaseTestcaseClass.java
+++ src/test/java/testBase/BaseTestcaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -13,11 +14,14 @@ import org.apache.logging.log4j.LogManager;
 //Logger Package from apache  core
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -33,22 +37,40 @@ public class BaseTestcaseClass {
 		prop = new Properties();
 		FileInputStream propFile = new FileInputStream("./src//test//resources//config.properties");
 		prop.load(propFile);
-		
-		System.out.println("Os used to run tests : "+os);
+
 		logger = LogManager.getLogger(this.getClass());
-		switch(browser.toLowerCase()) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-		default:
-			System.out.println("Invalid Browser !!");
-			return;
+		
+		if (prop.getProperty("execution_env").equals("local")) {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			default:
+				System.out.println("Invalid Browser !!");
+				return;
+			}
+		} else {
+			DesiredCapabilities cap = new DesiredCapabilities();	
+			switch(os.toLowerCase()) {
+			case "window" : cap.setPlatform(Platform.WINDOWS); break;
+			case "linux" : cap.setPlatform(Platform.LINUX); break;
+			case "mac" : cap.setPlatform(Platform.MAC); break;			
+			default : System.out.println("Invalid OS"); return;
+			}
+			switch(browser.toLowerCase()) {
+				case "chrome" : cap.setBrowserName("chrome"); break;
+				case "edge" : cap.setBrowserName("MicrosoftEdge"); break;
+				case "firefox" : cap.setBrowserName("firefox"); break;	
+				default : System.out.println("Inavlid Browser"); return;
+			}
+			String hubUrl = "http://localhost:4444/wd/hub";
+			driver = new RemoteWebDriver(new URL(hubUrl), cap);
 		}
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
@@ -56,6 +78,7 @@ public class BaseTestcaseClass {
 		String appUrl = prop.getProperty("appurl"); 
 		driver.get(appUrl);
 	}
+	
 	@AfterClass(groups= {"Sanity","Regression","Datadriven","Master"})
 	public void tearDown() throws InterruptedException {
 		Thread.sleep(5000);
